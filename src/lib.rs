581c52100f799e438bc72c0f2a57fd5bc01dad5b
grtcdr <ba.tahaaziz@gmail.com>
2021-06-23 11:19:05 +0100

    lib.rs: add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::path::PathBuf;
 
 mod extra;
 
+/// This function should return a list of connected PCI devices.
 pub fn get_pci_addresses(absolute_path: bool) -> Vec<PathBuf> {
     let mut address_list: Vec<PathBuf> = Vec::new();
     let addresses = list_dir_entries(std::path::Path::new("/sys/bus/pci/devices"));
@@ -20,6 +21,11 @@ pub fn get_pci_addresses(absolute_path: bool) -> Vec<PathBuf> {
     return address_list;
 }
 
+/// This function should return the `class` associated with the given `address`
+/// 
+/// Example:
+/// - `skip_over_prefix = false` -> `0x030000`
+/// - `skip_over_prefix = true` -> `030000`
 pub fn get_class(address: &PathBuf, skip_over_prefix: bool) -> Result<String, ()> {
     if let Ok(mut str) = std::fs::read_to_string(address.join("class")) {
         if skip_over_prefix {
@@ -32,6 +38,12 @@ pub fn get_class(address: &PathBuf, skip_over_prefix: bool) -> Result<String, ()
     Err(())
 }
 
+
+/// This function should return the `device` associated with the given `address`
+/// 
+/// Example:
+/// - `skip_over_prefix = false` -> `0x3ea0`
+/// - `skip_over_prefix = true` -> `3ea0`
 pub fn get_device(address: &PathBuf, skip_over_prefix: bool) -> Result<String, ()> {
     if let Ok(mut str) = std::fs::read_to_string(address.join("device")) {
         if skip_over_prefix {
@@ -44,6 +56,11 @@ pub fn get_device(address: &PathBuf, skip_over_prefix: bool) -> Result<String, (
     Err(())
 }
 
+/// This function should return the `vendor` associated with the given `address`
+/// 
+/// Example:
+/// - `skip_over_prefix = false` -> `0x8086`
+/// - `skip_over_prefix = true` -> `8086`
 pub fn get_vendor(address: &PathBuf, skip_over_prefix: bool) -> Result<String, ()> {
     if let Ok(mut str) = std::fs::read_to_string(address.join("vendor")) {
         if skip_over_prefix {
@@ -56,6 +73,11 @@ pub fn get_vendor(address: &PathBuf, skip_over_prefix: bool) -> Result<String, (
     Err(())
 }
 
+/// This function should return the `revision` associated with the given `address`
+/// 
+/// Example:
+/// - `skip_over_prefix = false` -> `0x02`
+/// - `skip_over_prefix = true` -> `02`
 pub fn get_revision(address: &PathBuf, skip_over_prefix: bool) -> Result<String, ()> {
     if let Ok(mut str) = std::fs::read_to_string(address.join("revision")) {
         if skip_over_prefix {
