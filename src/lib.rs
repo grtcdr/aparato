a7a3f8585f67034e2aea5115a5f616783887946f
grtcdr <ba.tahaaziz@gmail.com>
2021-06-25 17:26:37 +0100

    ➕Include cfg_if to check target_os when compiling
    - cfg_if is our first dependency, hopefully there won't be too many in
    the future.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
+cfg_if::cfg_if! {
+    if #[cfg(target_os = "linux")] {
+        pub mod linux;
+        pub type PCIDevice = linux::LinuxPCIDevice;
+
+    } else {
+        compile_error!("aparato does not support this platform, at least not yet.");
+    }
+}
+
 pub mod classes;
 mod extra;
-pub mod linux;
 pub mod traits;
-pub type PCIDevice = linux::LinuxPCIDevice;
