dbe7fabde736a16144eb365dd2016b1519aadcba
grtcdr <ba.tahaaziz@gmail.com>
2021-06-25 21:19:48 +0100

    Add placeholders for to-be-implemented platforms

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,15 @@ cfg_if::cfg_if! {
     if #[cfg(target_os = "linux")] {
         pub mod linux;
         pub type PCIDevice = linux::LinuxPCIDevice;
-
+    } else if #[cfg(target_os = "macos")] {
+        pub mod macos;
+        pub type PCIDevice = macos::MacOSPCIDevice;
+    } else if #[cfg(target_os = "netbsd")] {
+        pub mod netbsd;
+        pub type PCIDevice = netbsd::NetBSDPCIDevice;
+    } else if #[cfg(target_os = "windows")] {
+        pub mod windows;
+        pub type PCIDevice = windows::WindowsPCIDevice;
     } else {
         compile_error!("aparato does not support this platform, at least not yet.");
     }
---
+++ src/netbsd/mod.rs
@@ -0,0 +1,118 @@
+use crate::classes::*;
+use crate::traits::*;
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub struct NetBSDPCIDevice {
+    path: PathBuf,
+    address: String,
+    class_id: String,
+    class_name: String,
+    vendor_id: String,
+    vendor_name: String,
+    device_id: String,
+    device_name: String,
+    numa_node: isize,
+}
+
+impl Properties for NetBSDPCIDevice {
+    fn new(path: &str) -> Self {
+        todo!()
+    }
+
+    fn init(&mut self) {
+        todo!()
+    }
+
+    fn path(&self) -> PathBuf {
+        self.path.to_owned()
+    }
+
+    fn address(&self) -> String {
+        self.address.to_owned()
+    }
+
+    fn class_id(&self) -> String {
+        self.class_id.to_owned()
+    }
+
+    fn vendor_id(&self) -> String {
+        self.vendor_id.to_owned()
+    }
+
+    fn device_id(&self) -> String {
+        self.device_id.to_owned()
+    }
+
+    fn numa_node(&self) -> isize {
+        self.numa_node.to_owned()
+    }
+
+    fn class_name(&self) -> String {
+        self.class_name.to_owned()
+    }
+
+    fn vendor_name(&self) -> String {
+        self.vendor_name.to_owned()
+    }
+
+    fn device_name(&self) -> String {
+        self.device_name.to_owned()
+    }
+
+    fn set_path(&mut self, p: PathBuf) {
+        self.path = p;
+    }
+
+    fn set_address(&mut self) {
+        todo!()
+    }
+
+    fn set_class_id(&mut self) {
+        todo!()
+    }
+
+    fn set_vendor_id(&mut self) {
+        todo!()
+    }
+
+    fn set_device_id(&mut self) {
+        todo!()
+    }
+
+    fn set_numa_node(&mut self) {
+        todo!()
+    }
+
+    fn set_class_name(&mut self) {
+        todo!()
+    }
+
+    fn set_vendor_name(&mut self, name: String) {
+        self.vendor_name = name;
+    }
+
+    fn set_device_name(&mut self, name: String) {
+        self.device_name = name;
+    }
+}
+
+impl std::fmt::Display for NetBSDPCIDevice {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.address)
+    }
+}
+
+impl Fetch for NetBSDPCIDevice {
+    fn fetch() -> Vec<NetBSDPCIDevice> {
+        todo!()
+    }
+
+    fn fetch_by_class(class: DeviceClass) -> Vec<NetBSDPCIDevice> {
+        todo!()
+    }
+
+    fn fetch_gpus() -> Vec<NetBSDPCIDevice> {
+        todo!()
+    }
+}
